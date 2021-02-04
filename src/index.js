5fb6fa8a2657d131a3c8a98c4479073cb8557bcb
intae92 <kangit2013@gmail.com>
2021-02-04 22:01:02 +0900

    setup React, Firebase, Router, env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
+import firebase from "./firebase";
 
-import App from "./App";
-
+console.log(firebase);
 ReactDOM.render(
   <React.StrictMode>
     <App />
